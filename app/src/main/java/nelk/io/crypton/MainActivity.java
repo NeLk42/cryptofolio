6cacfef98d1f16c5b7a511025c0552e0698c96f4
NeLk42 <nelk42@gmail.com>
2017-09-09 21:47:44 +0100

    Changed from ConstraintLayout to GridLayout, getting ready for using RecyclerView.

--- app/src/main/java/nelk/io/crypton/MainActivity.java
+++ app/src/main/java/nelk/io/crypton/MainActivity.java
@@ -2,8 +2,16 @@ package nelk.io.crypton;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+
+import nelk.io.crypton.recyclerview.CoinAdapter;
+import nelk.io.crypton.retrofit.BittrexAPI;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String TAG = MainActivity.class.getSimpleName();
+
+    private RecyclerView mRecyclerView;
+    private CoinAdapter mCoinAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
